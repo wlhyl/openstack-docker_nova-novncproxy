84f8d6fe27df52fbb1c16a83df40bb8a2f136e97
lzh <1216083447@qq.com>
2015-08-13 11:43:18 +0800

    Dockerfile

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 - /opt/openstack/log/nova-novncproxy/: /var/log/nova/
 
 # 启动nova-novncproxy
+```bash
 docker run -d --name nova-novncproxy \
     -v /opt/openstack/nova-novncproxy/:/etc/nova \
     -v /opt/openstack/log/nova-novncproxy/:/var/log/nova/ \
@@ -19,3 +20,4 @@ docker run -d --name nova-novncproxy \
     -e VNCSERVER_PROXYCLIENT_ADDRESS=10.64.0.52 \
     -e MY_IP=10.64.0.52 \
     10.64.0.50:5000/lzh/nova-consoleauth:kilo
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -15,11 +15,6 @@ if [ -z "$RABBIT_PASSWORD" ];then
   exit 1
 fi
 
-if [ -z "$VNCSERVER_PROXYCLIENT_ADDRESS" ];then
-  echo "error: VNCSERVER_PROXYCLIENT_ADDRESS not set"
-  exit 1
-fi
-
 if [ -z "$MY_IP" ];then
   echo "error: MY_IP not set. my_ip use management interface IP address."
   exit 1
@@ -29,8 +24,6 @@ CRUDINI='/usr/bin/crudini'
 
 if [ ! -f /etc/nova/.complete ];then
     cp -rp /nova/* /etc/nova
-
-    chown nova:nova /var/log/nova/
     
     $CRUDINI --set /etc/nova/nova.conf DEFAULT rpc_backend rabbit
 
@@ -40,7 +33,7 @@ if [ ! -f /etc/nova/.complete ];then
 
     $CRUDINI --set /etc/nova/nova.conf DEFAULT vncserver_listen 0.0.0.0
 
-    $CRUDINI --set /etc/nova/nova.conf DEFAULT vncserver_proxyclient_address $VNCSERVER_PROXYCLIENT_ADDRESS
+    $CRUDINI --set /etc/nova/nova.conf DEFAULT vncserver_proxyclient_address $MY_IP
     
     $CRUDINI --set /etc/nova/nova.conf DEFAULT my_ip $MY_IP
     
@@ -50,5 +43,7 @@ if [ ! -f /etc/nova/.complete ];then
 
     touch /etc/nova/.complete
 fi
+
+chown -R nova:nova /var/log/nova/
 
 /usr/bin/supervisord -n
